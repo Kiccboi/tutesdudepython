dae7d390fd174333001739c132f721932ff44053
Kiccboi <muk3612@gmail.com>
2025-04-02 22:19:43 +0530

    Create test1.py

---
+++ test1.py
@@ -0,0 +1,16 @@
+Task 1 : -
+x=int(input("Enter the first number: "))
+y=int(input("Enter the Second number: "))
+print("\n")
+a=x+y
+s=x-y
+m=x*y
+d=x / y
+print(f"Addition: {a} \n Subtraction: {s} \n Multiplication: {m} \n Division: {d}")
+
+-------------------------------------------------------------------------------------------------------------------
+Task 2 : -
+x=input("Enter your first name: " )
+y= input("Enter your second name:" )
+z=x+' '+y+'!'
+print(f"Hello, {z} Welcome to the python program.")
