02cb70f8d83ff2a0d34b3a461c66bee5913f8789
Kiccboi <muk3612@gmail.com>
2025-04-15 19:55:39 +0530

    Create test2.py

---
+++ test2.py
@@ -0,0 +1,11 @@
+#Task 1
+x=int(input("Enter a number: "))
+if (x%2==0):
+    print(f"{x} is an Even number")
+else:
+    print(f"{x} is an Odd number")
+#Task 2
+sum=0
+for i in range(1,51):
+    sum=sum+i
+print(f"The sum of numbers from 1 to 50 is : {sum}")
